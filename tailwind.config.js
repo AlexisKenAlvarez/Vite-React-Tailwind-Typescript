113d25be312e8901902f8fcc20281e2465241504
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-09-19 13:49:09 +0800

    fixed template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        primary: ['Knewave'],
-        secondary: ['Kiwi Maru']
-      },
-      colors: {
-        bg: '#D3FFB4',
-        primary: '#6DAE3F'
-      }
+      fontFamily: {},
+      colors: {},
     },
   },
   plugins: [],
-}
-
+};
