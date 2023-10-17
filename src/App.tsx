68983f9296392ef9ecdf18783f9661d42451332e
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-10-17 12:19:37 +0800

    Template

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import Footer from "./components/Footer";
-import About from "./views/About";
-import Hero from "./views/Hero";
-import Tokenomics from "./views/Tokenomics";
-
 const App = () => {
   return (
-    <div className="w-full h-auto my-bg bg-bg">
-      <Hero />
-      <About />
-      <Tokenomics />
-      <Footer />
+    <div className="w-full h-auto">
+      Enter
     </div>
   );
-};
+}
 
 export default App;
