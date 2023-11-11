c58335121dfc277a6f75782ed1c4964f34790ddf
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-11-11 10:16:19 +0800

    added cn and button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,28 @@
-import { FunctionComponent, ReactNode } from "react"
+import { FunctionComponent, ReactNode } from "react";
+import { cn } from "../utils";
 
-interface ButtonProps {
-    children: ReactNode,
-    className?: string,
-    onClick?: () => void
+interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
+  children: ReactNode;
+  className?: string;
 }
 
-export const Button: FunctionComponent<ButtonProps> = ({ children, className, onClick }) => {
-    return (
+export const Button: FunctionComponent<ButtonProps> = ({
+  children,
+  className,
 
-        <button className={`${className}`} onClick={onClick}>
-            <h1 className="">
-                {children}
-            </h1>
-        </button>
+  ...props
+}) => {
+  return (
+    <button
+      className={cn(
+        "px-6 py-3 relative border opacity-60 font-primary text-center text-sm rounded-sm hover:bg-grn transition-all ease-in-out duration-300 hover:opacity-100 hover:text-bl hover:border-grn hover:font-bold",
+        className
+      )}
+      {...props}
+    >
+      <h1 className="">{children}</h1>
+    </button>
+  );
+};
 
-    )
-}
+export default Button;
